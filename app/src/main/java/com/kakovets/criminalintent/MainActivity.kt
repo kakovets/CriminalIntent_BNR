8c9355e795afad69dd79384ac5652914980b9cc1
kakovets <victorentino@gmail.com>
2023-03-26 00:16:22 +0200

    Initial commit

---
+++ app/src/main/java/com/kakovets/criminalintent/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.kakovets.criminalintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = CrimeListFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+//        CrimeFragment().onCreate(Bundle())
+    }
+}
