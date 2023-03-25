70c89c13105c84804094b4640f638290b9cf4d62
kakovets <victorentino@gmail.com>
2023-03-26 00:36:42 +0200

    Added layout for RecyclerView and initialization of RecyclerView

--- app/src/main/java/com/kakovets/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/kakovets/criminalintent/CrimeListFragment.kt
@@ -2,12 +2,20 @@ package com.kakovets.criminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment: Fragment() {
+
+    private lateinit var crimeRecyclerView: RecyclerView
+
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
     }
@@ -15,6 +23,18 @@ class CrimeListFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+        crimeRecyclerView = view.findViewById(R.id.recyclerView)
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        return view
+
     }
 
     companion object {
