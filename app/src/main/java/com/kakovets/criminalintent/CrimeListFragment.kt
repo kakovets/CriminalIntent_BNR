b1e120b25ede94eb3db4ee4832b71b0f9f68cd71
kakovets <victorentino@gmail.com>
2023-03-28 02:49:03 +0300

    Replaced LinearLayout to ConstrainedLayout. Added photo marker of isSolved property.

--- app/src/main/java/com/kakovets/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/kakovets/criminalintent/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -55,6 +56,7 @@ class CrimeListFragment: Fragment() {
         private lateinit var crime: Crime
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val crimeSolvedImageView: ImageView = itemView.findViewById(R.id.imageView_crime_solved)
 
         init {
             itemView.setOnClickListener(this)
@@ -64,6 +66,14 @@ class CrimeListFragment: Fragment() {
             this.crime = crime
             titleTextView.text = this.crime.title
             dateTextView.text = this.crime.date.toString()
+//            For some reason in the case of fast scrolling all imageViews become invisible.
+//            Maybe because of reusing holders.
+//            if (!crime.isSolved) crimeSolvedImageView.isVisible = false
+            crimeSolvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
