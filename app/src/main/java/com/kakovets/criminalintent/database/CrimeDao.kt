8c83080688de384102775a922e1ca1070f68f987
kakovets <victorentino@gmail.com>
2023-03-28 14:46:48 +0300

    Added database package with Room ORM: TypeConverters, Database classes and Dao interface.

---
+++ app/src/main/java/com/kakovets/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,14 @@
+package com.kakovets.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.kakovets.criminalintent.Crime
+import java.util.UUID
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * from crime")
+    fun getCrimes(): List<Crime>
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime?
+}
