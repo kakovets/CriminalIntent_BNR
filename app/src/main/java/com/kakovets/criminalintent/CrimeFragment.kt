1f1dbfd0e9f23cb12f3c12f3eae871b8abda7d51
kakovets <victorentino@gmail.com>
2023-03-30 12:18:11 +0300

    Added ViewModel for fragment with details of the crime. Added observer for the LiveData of this ViewModel, so now CrimeFragment with details opens by clicking on the crime from the list.

--- app/src/main/java/com/kakovets/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/kakovets/criminalintent/CrimeFragment.kt
@@ -11,6 +11,8 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import java.util.UUID
 
 private const val ARG_CRIME_ID = "crime_id"
@@ -22,12 +24,16 @@ class CrimeFragment: Fragment() {
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
+    private val crimeDetailViewModel: CrimeDetailViewModel by lazy {
+        ViewModelProvider(this)[CrimeDetailViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
         val crimeID: UUID = UUID.fromString(arguments?.getString(ARG_CRIME_ID))
         Log.d(TAG, crimeID.toString())
+        crimeDetailViewModel.loadCrime(crimeID)
     }
 
     override fun onCreateView(
@@ -45,6 +51,19 @@ class CrimeFragment: Fragment() {
             isEnabled = false
         }
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeDetailViewModel.crimeLivedata.observe(
+            viewLifecycleOwner,
+            Observer { crime ->
+                crime?.let {
+                    this.crime = crime
+                    updateUI()
+                }
+            }
+        )
     }
 
     override fun onStart() {
@@ -69,6 +88,15 @@ class CrimeFragment: Fragment() {
             setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
             }
+        }
+    }
+
+    private fun updateUI() {
+        titleField.setText(crime.title)
+        dateButton.text = crime.date.toString()
+        solvedCheckBox.apply {
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
         }
     }
 
